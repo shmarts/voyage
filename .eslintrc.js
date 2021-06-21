c14faaec8a2cca46ae9b4e103716331c845d56c1
Martin Rojo <wat.martinrojo@gmail.com>
2021-06-21 23:48:01 +0100

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,14 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
   extends: [
-    'eslint:recommended',
-    'standard',
-    'prettier',
-    'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-recommended',
+    'eslint:recommended',
     '@vue/typescript/recommended',
     '@vue/prettier',
     '@vue/prettier/@typescript-eslint',
   ],
   plugins: ['@typescript-eslint'],
   env: {
+    browser: true,
     es6: true,
     node: true,
   },
