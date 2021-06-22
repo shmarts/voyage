2206d1553b16d30852c1771971eb55a20f743bfb
Martin Rojo <wat.martinrojo@gmail.com>
2021-06-22 22:12:33 +0100

    setup chrome extension

--- packages/client.chrome-extension/src/main.ts
+++ packages/client.chrome-extension/src/main.ts
@@ -3,3 +3,21 @@ import App from './App.vue'
 import './index.css'
 
 createApp(App).mount('#app')
+
+// const MOUNT_EL_ID = 'voyage_el'
+
+// let mountEl = document.getElementById(MOUNT_EL_ID)
+// if (mountEl) {
+//   mountEl.innerHTML = ''
+// }
+// mountEl = document.createElement('div')
+// mountEl.setAttribute('id', MOUNT_EL_ID)
+// document.body.appendChild(mountEl)
+
+// const vm = createApp(App).mount(mountEl)
+
+// chrome.runtime.onMessage.addListener((message) => {
+//   if (message.toggleVisible) {
+//     ;(vm as any).visible = !(vm as any).visible
+//   }
+// })
--- packages/client.chrome-extension/vite.config.js
+++ packages/client.chrome-extension/vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { chromeExtension } from 'vite-plugin-chrome-extension'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), chromeExtension()],
   resolve: {
     alias: {
       '@voyage': path.resolve(__dirname, '../'),
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: 'src/manifest.json',
     },
   },
 })
