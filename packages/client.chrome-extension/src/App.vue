b1fb56fe6e104e90003bbcc515aba362533ede9b
Martin Rojo <wat.martinrojo@gmail.com>
2021-06-23 13:24:41 +0100

    add some content

--- packages/client.chrome-extension/src/App.vue
+++ packages/client.chrome-extension/src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="font-bold">voyage</div>
+  <div class="fixed inset-0 z-50 p-1 font-bold border-4 border-red-500 pointer-events-none">
+    voyage
+  </div>
+  <div class="p-10">
+    <!-- Sample webpage in background -->
+    <p v-for="i in 100" :key="i" class="mb-8">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate corporis alias neque
+      eveniet quia eos officia veritatis voluptatibus quaerat ducimus laborum perferendis dicta
+      maxime, deleniti sunt! Amet praesentium quia quibusdam labore quas unde sed ipsa, nihil est
+      eaque iure odio totam laudantium voluptatum iusto repellendus earum! In officia odio
+      consectetur illo, minima labore.
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
