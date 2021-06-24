bf3c7e7a3aa2ef3216626bac3e15c03d19708253
Martin Rojo <wat.martinrojo@gmail.com>
2021-06-24 19:34:48 +0100

    feat: init ws

--- packages/service.voyage/src/index.ts
+++ packages/service.voyage/src/index.ts
@@ -1,3 +1,18 @@
-import { helloWorld } from '@voyage/lib'
+import { createServer } from 'http'
+import WebSocket from 'ws'
+import config from './config'
 
-helloWorld('Node')
+const server = createServer()
+const wss = new WebSocket.Server({ server })
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    console.log('received', message)
+  })
+
+  ws.send('you are connected')
+})
+
+server.listen(config.port, () => {
+  console.log(`Listening on ${config.port}`)
+})
