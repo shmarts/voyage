ee2dde51bf83e8223b27f56316c5e49505e3385e
Martin Rojo <wat.martinrojo@gmail.com>
2021-06-26 15:39:43 +0100

    feat: view model

---
+++ packages/service.voyage/src/models/view.ts
@@ -0,0 +1,30 @@
+import Client from '@models/client'
+import { Position } from '@types'
+
+type State = {
+  client: Client
+  position: Position
+}[]
+
+export default class View {
+  constructor(id: string) {
+    this.id = id
+    this.state = []
+  }
+
+  id: string
+  state: State
+
+  clientJoin(client: Client): void {
+    this.state.push({ client, position: [0, 0] })
+  }
+
+  clientMove(client: Client, position: Position): void {
+    const i = this.getClientIndex(client)
+    this.state[i].position = position
+  }
+
+  getClientIndex(client: Client): number {
+    return this.state.findIndex(({ client: { id } }) => id === client.id)
+  }
+}
