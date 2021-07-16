5e1da19a834f5c559fafb006582b6260312ed1cf
Martin Rojo <wat.martinrojo@gmail.com>
2021-07-16 21:40:46 +0100

    Create useMousePosition.ts

---
+++ packages/client.chrome-extension/src/hooks/useMousePosition.ts
@@ -0,0 +1,20 @@
+import { Ref } from 'vue'
+import { Position } from '@voyage/types'
+
+export default (): { position: Ref<Position> } => {
+  const position = ref<Position>([0, 0])
+
+  const handler = (event: MouseEvent) => {
+    position.value = [event.pageX, event.pageY]
+  }
+
+  window.addEventListener('mousemove', handler)
+  window.addEventListener('dragover', handler)
+
+  onUnmounted(() => {
+    window.removeEventListener('mousemove', handler)
+    window.removeEventListener('dragover', handler)
+  })
+
+  return { position }
+}
